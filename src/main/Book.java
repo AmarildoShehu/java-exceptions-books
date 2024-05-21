eaae5a3e35a66cfa9ccc8c96d1b2ef4f5681ea67
Amarildo <you@example.com>
2024-05-21 14:23:13 +0200

    created attribute and gutter and setter

--- src/main/Book.java
+++ src/main/Book.java
@@ -1,7 +1,62 @@
 package main;
 
 public class Book {
-    public static void main(String[] args) {
+        private String title;
+        private int numberOfPages;
+        private String author;
+        private String publisher;
 
+        public Book(String title, int numberOfPages, String author, String publisher) {
+            setTitle(title);
+            setNumberOfPages(numberOfPages);
+            setAuthor(author);
+            setPublisher(publisher);
+        }
+
+    public String getTitle() {
+        return title;
     }
+
+    public void setTitle(String title) {
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Il titolo non puo essere vuoto ");
+        }
+        this.title = title;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        if (numberOfPages <= 0) {
+            throw new IllegalArgumentException("Le pagine devono essere maggiori di 0 ");
+        }
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if (author == null || author.isEmpty()) {
+            throw new IllegalArgumentException("l'autore non puo essere vuoto ");
+        }
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        if (publisher == null || publisher.isEmpty()) {
+            throw new IllegalArgumentException("Il dato non puo essere vuoto ");
+        }
+        this.publisher = publisher;
+    }
+
+
 }
+
